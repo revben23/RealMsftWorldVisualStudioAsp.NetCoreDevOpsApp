3f0d7da6c255ac2bff64687332a0074e99b68c07
revben23 <1989revben23@users.noreply.github.com>
2018-01-05 12:15:29 -0500

    I cannot be bothered

---
+++ RealMsftWorldVisualStudioAsp.NetCoreDevOpsApp/wwwroot/js/index.js
@@ -0,0 +1,25 @@
+﻿
+
+/// <reference path="jquery-3.2.1.min.js" />
+
+
+
+//alert("Hello Microsoft World");
+console.log("Hello Microsoft World");
+
+var form = document.getElementById("theForm");
+form.hidden = false;
+//var form = $("theForm");
+//form.hidden = true;
+
+var submit = document.getElementById("submit");
+submit.addEventListener("click", function () {
+   alert("Submititng Message");
+
+});
+
+var label = document.getElementById("labelFirst")
+label.addEventListener("click", function () {
+    alert("You Clicked Me");
+
+});
